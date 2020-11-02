3555934fa6393be3e9fece90c82b169a928ea1ff
Matte Sette <msette8@gmail.com>
2020-11-02 22:58:43 +0100

    init

---
+++ src/home/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center"><p className="text-green-500 text-4xl">Hello</p></div>
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {},
+  purge: [
+    'src/**/*.js',
+    'src/**/*.jsx',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
